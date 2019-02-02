44f0e8920ba8185c446f32413fe9d374a502e17d
Maciej Bukowski <ma2ciek@gmail.com>
2019-02-02 13:49:09 +0100

    Changed the default file to the `index.ts` that reexports module, component and typings.

---
+++ src/ckeditor/index.ts
@@ -0,0 +1,8 @@
+/**
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+export * from './ckeditor';
+export * from './ckeditor.component';
+export * from './ckeditor.module';
