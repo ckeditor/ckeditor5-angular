dde33ea79e7842732d3f5e3248052032ec7d1193
Maciej Bukowski <ma2ciek@gmail.com>
2019-10-04 12:32:58 +0200

    Prevented ckeditor component from calling CVA onChange when the change comes from the CVA.

--- src/app/demo-form/demo-form.component.spec.ts
+++ src/app/demo-form/demo-form.component.spec.ts
@@ -55,7 +55,7 @@ describe( 'DemoFormComponent', () => {
 
 			fixture.detectChanges();
 
-			expect( component.formDataPreview ).toEqual( '{"name":null,"surname":null,"description":""}' );
+			expect( component.formDataPreview ).toEqual( '{"name":null,"surname":null,"description":null}' );
 
 			done();
 		} );
