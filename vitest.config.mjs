cb9e14747b72a5c468bd9fef3d8ccbe4f8a492d1
Przemysław Zań <przemyslaw.zan@gmail.com>
2026-05-20 16:50:51 +0200

    Bumped deps.

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -94,6 +94,7 @@ export default defineConfig( {
 				'src/generated/**',
 				'src/**/*.spec.ts',
 				'src/**/*.module.ts',
+				'src/app/**',
 				'src/editor/**',
 				'src/environments/**',
 				'src/main.ts',
